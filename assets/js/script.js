1621eec475a4fd4c6ab1a978ff2bead9fc743e71
vargasdm <112888008+vargasdm@users.noreply.github.com>
2022-11-02 20:16:32 -0400

    added psuedocode for html, css, and javascript

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+// want an event listener that starts the game function when the button is clicked
+
+// want an event listener that starts a timer when the start game button
+
+// need a function for a timer
+    // need timer to subract time when incorrect answer is chosen            not sure if this should be in timer function or start game function??????
+    //  when timer hits 0, it shows the game over screen
+
+// need a function that renders a question and answers
+    // answers need to be list items that are presented as buttons
+        // want to create each question as an object variable
+            // want all of the answers to be properties that have the value of either true or false             (should boolean be held in data attribute or s it fine to set the property as the answer)
+                // should put every question variable into an array
+
+    // needs to render "correct" or "incorrect" you after you click an answer
+    // the sstring will be rendered at the bottom or under element that holds the answeers 
+        // create a function for this ??
+
+
+    // after a question is answered correctly the score needs to increase
+    // timer to deduct time when incorrect answer is chosen
+        // write a function for this ??????
+
+    // after question is answered present the next question
+        // could i just rerun the function but somehow change the question variable that I am calling??? or probablyu better to create a loop that moves through each question in array
+
+    // when all questions are answered, i want to be brought to the game over screen
+
+
+// need an event listener for game over screeen that when you submit your info
+    // needs to store name data and score in local memory
+
+// need a function for when the highscore button is clicked
+    // it will need to grab the previous highscores and naems from the local storage and present them on the page
+
