91c6b7fc07bd31b178e81018fec02dfcec1e159a
vargasdm <112888008+vargasdm@users.noreply.github.com>
2022-11-05 10:31:00 -0400

    added variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,138 @@
-// want an event listener that starts the game function when the button is clicked
+var startBtn = document.querySelector("#startBtn");
+var timeEl = document.querySelector("#timer");
+var gameOverScreen = document.querySelector("#gameOver")
+var questionScreen = document.querySelector("#askQuestion")
+var initials = document.querySelector("#initials");
+var saveBtn = document.querySelector("#saveBtn");
+var secondsLeft = 2;
 
-// want an event listener that starts a timer when the start game button
+// question array that will be moved through ass you answer them
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        options: [
+            "strings",
+            "booleans",
+            "prompts",
+            "numbers"
+        ],
+        answer: "prompts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within _____:",
+        options: [
+            "parentheses",
+            "curly brackets",
+            "colons",
+            "periods"
+        ],
+        answer: "parentheses"
+    },
+    {
+        title: "Arrays in Javascript can be used to store _____:",
+        options: [
+            "strings",
+            "booleans",
+            "numbers",
+            "all of the above"
+        ],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed with _____ when being assigned to a variable:",
+        options: [
+            "commas",
+            "quotations",
+            "parentheses",
+            "curly brackets"
+        ],
+        answer: "quotations"
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        options: [
+            "for loops",
+            "if statments",
+            "Terminal / Bash",
+            "console.log"
+        ],
+        answer: "console.log"
+    }
+]
+
+
+//  need to hide game over screeen until end of game
+gameOverScreen.setAttribute("style", "display: none");
+questionScreen.setAttribute("style", "display: none");
+
+function startGame() {
+// need to start timer when start game function is started
+setTime();
+
+// need start button to disapper when startGame function starts
+startBtn.setAttribute("style", "display: none");
+
+// need to display questions when start game function is started
+// questionDisplay();
+questionScreen.setAttribute("style", "display: block");
 
 // need a function for a timer
-    // need timer to subract time when incorrect answer is chosen            not sure if this should be in timer function or start game function??????
-    //  when timer hits 0, it shows the game over screen
+    // need timer to subract time when incorrect answer is chosen           
+    
+    function setTime() {
+        // Sets interval in variable
+        var timerInterval = setInterval(function() {
+          secondsLeft--;
+          timeEl.textContent = "Time left: " + secondsLeft;
+      
+        // if ()   
+
+          //  when timer hits 0, it shows the game over screen
+        if(secondsLeft === 0 || questions === questions[questions.length-1]) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+            // Calls function to show game over screen 
+            gameOverScreen.setAttribute("style", "display: block");
+            // hides timer
+            timeEl.setAttribute("style", "display: none");
+            // hides question screen
+            questionScreen.setAttribute("style", "display: none");
+          }
+      
+        }, 1000);
+      }
+
+
+      // need an event listener for game over screeen that when you submit your info
+    // needs to store name data and score in local memory
+saveBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    saveLastGrade();
+    renderLastGrade();
+    });
+
+    function saveLastGrade() {
+        // Save related form data as an object
+        var playerId = initials.value;
+        
+        // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
+        localStorage.setItem("playerId", JSON.stringify(playerId));
+
+        console.log(playerId);
+    }
+}
+
+
+
+
+
+// want an event listener that starts the game function when the button is clicked
+startBtn.addEventListener("click", startGame);
+
+
+
+
+
 
 // need a function that renders a question and answers
     // answers need to be list items that are presented as buttons
@@ -21,14 +149,9 @@
     // timer to deduct time when incorrect answer is chosen
         // write a function for this ??????
 
-    // after question is answered present the next question
-        // could i just rerun the function but somehow change the question variable that I am calling??? or probablyu better to create a loop that moves through each question in array
-
     // when all questions are answered, i want to be brought to the game over screen
 
 
-// need an event listener for game over screeen that when you submit your info
-    // needs to store name data and score in local memory
 
 // need a function for when the highscore button is clicked
     // it will need to grab the previous highscores and naems from the local storage and present them on the page
